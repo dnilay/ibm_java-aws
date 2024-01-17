8c9ddf203844d1a485853d2f4aa02c4badeb300e
Nilay <54245048+dnilay@users.noreply.github.com>
2024-01-17 12:46:25 +0530

    Add files via upload

--- guiter-app/src/main/java/com/ibm/example/App.java
+++ guiter-app/src/main/java/com/ibm/example/App.java
@@ -1,5 +1,6 @@
 package com.ibm.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.UUID;
 
@@ -8,80 +9,87 @@ import com.ibm.example.model.GuiterType;
 import com.ibm.example.repo.GuiterRepository;
 import com.ibm.example.repo.Inventory;
 
-public class App 
-{
-	private static Scanner scanner=null;
+public class App {
+	private static Scanner scanner = null;
 	private static GuiterRepository guiterRepository;
-	static
-	{
-		guiterRepository=new Inventory();
-		scanner=new Scanner(System.in);
+	static {
+		guiterRepository = new Inventory();
+		scanner = new Scanner(System.in);
 	}
-    public static void main( String[] args )
-    {
-       int choice=0;
-       do {
-    	   System.out.println("1. Add A New Guiter.");
-    	   System.out.println("2. Search A Guiter.");
-    	   System.out.println("3. Get Guiter.");
-    	   System.out.println("0. Exit.");
-    	   System.out.print("enter your choice: ");
-    	   choice=scanner.nextInt();
-    	   switch (choice) {
-		case 1:
-			System.out.print("Enter Guiter Price: ");
-			double p=scanner.nextDouble();
-			System.out.print("Enter Builder Name: ");
-			String b=scanner.next();
-			System.out.print("Enter Model: ");
-			String m=scanner.next();
-			System.out.print("Enter Guiter Type: ");
-			String t=scanner.next();
-			System.out.print("Enter BackWood: ");
-			String bw=scanner.next();
-			System.out.println("Enter TopWood: ");
-			String tw=scanner.next();
-			guiterRepository.addGuiters(new Guiter(UUID.randomUUID().toString(), p, b, m, GuiterType.valueOf(t), bw, tw));
-			break;
-		case 2:
-			System.out.print("Enter Serial Number: ");
-			String s=scanner.next();
-			Guiter guiter=guiterRepository.getGuiter(s);
-			if(guiter!=null)
-			{
-				System.out.println("Found: "+guiter);
+
+	public static void main(String[] args) {
+		int choice = 0;
+		do {
+			System.out.println("1. Add A New Guiter.");
+			System.out.println("2. Search A Guiter.");
+			System.out.println("3. Get Guiter.");
+			System.out.println("0. Exit.");
+			System.out.print("enter your choice: ");
+			choice = scanner.nextInt();
+			switch (choice) {
+			case 1:
+				double p = 20;
+				try {
+					System.out.print("Enter Guiter Price: ");
+					p = scanner.nextDouble();
+
+				} catch (InputMismatchException e) {
+					System.out.println("invalid price");
+					
+				}
+				System.out.print("Enter Builder Name: ");
+				String b = scanner.next();
+				scanner.next();
+				System.out.print("Enter Model: ");
+				String m = scanner.next();
+				System.out.print("Enter Guiter Type: ");
+				String t = scanner.next();
+				GuiterType gType=GuiterType.AQUASTIC;
+				try {
+					gType = GuiterType.valueOf(t);
+					System.out.print("Enter BackWood: ");
+
+				} catch (IllegalArgumentException e) {
+					System.out.println("invalid guiter type");
+					
+				}
+				System.out.println("Enter Back Wood: ");
+				String bw = scanner.next();
+				System.out.println("Enter TopWood: ");
+				String tw = scanner.next();
+				guiterRepository
+						.addGuiters(new Guiter(UUID.randomUUID().toString(), p, b, m, gType, bw, tw));
+				break;
+			case 2:
+				System.out.print("Enter Serial Number: ");
+				String s = scanner.next();
+				Guiter guiter = guiterRepository.getGuiter(s);
+				if (guiter != null) {
+					System.out.println("Found: " + guiter);
+				} else {
+					System.out.println("Guiter not found");
+				}
+				break;
+			case 3:
+				System.out.print("Enter Serial Number: ");
+				String s1 = scanner.next();
+				Guiter g = guiterRepository.getGuiter(s1);
+				Guiter g1 = guiterRepository.searchGuiter(g);
+				if (g1 == null) {
+					System.out.println("invalid guiter object");
+				} else {
+					System.out.println(g1);
+				}
+				break;
+			case 0:
+				System.exit(0);
+
+			default:
+				System.out.println("Invalid choice..");
+				break;
 			}
-			else
-			{
-				System.out.println("Guiter not found");
-			}
-			break;
-		case 3:
-			System.out.print("Enter Serial Number: ");
-			String s1=scanner.next();
-			Guiter g=guiterRepository.getGuiter(s1);
-			Guiter g1=guiterRepository.searchGuiter(g);
-			if(g1==null)
-			{
-				System.out.println("invalid guiter object");
-			}
-			else
-			{
-				System.out.println(g1);
-			}
-			break;
-		case 0:
-			System.exit(0);
-		
-			
-		default:
-			System.out.println("Invalid choice..");
-			break;
-		}
-    	   
-       }while(choice!=0);
-       
-       
-       
-    }
+
+		} while (choice != 0);
+
+	}
 }
