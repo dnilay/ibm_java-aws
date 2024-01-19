f90d7ddfe1bcd047205525fd36440493487354ab
Nilay <54245048+dnilay@users.noreply.github.com>
2024-01-19 16:43:24 +0530

    Add files via upload

--- customer-management-system/src/main/java/com/ibm/model/InstructorDetail.java
+++ customer-management-system/src/main/java/com/ibm/model/InstructorDetail.java
@@ -7,6 +7,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 @Table(name="instructor_detail")
 public class InstructorDetail {
